760d3882a10e1a75b771848734bb89650522a490
sammav21 <sammav96@gmail.com>
2023-09-13 13:39:21 -0400

    Potential Welcome created, Beginning home page. Google fonts + react icons added

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,15 +1,19 @@
 import '@styles/globals.scss';
 import {Nav, Footer} from '../components/index';
+import {Poppins} from 'next/font/google';
 
 export const metadata = {
   title: 'ecommerce',
   description: 'project'
 }
+
+const poppins = Poppins({subsets: ['latin'], weight: ['400']});
+
 const RootLayout = ({children}) => {
   return (
     <html lang='en'>
       <body>
-        <main className='app'>
+        <main className='app' style={poppins.style}>
           <Nav />
           {children}
           <Footer />
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,7 +1,19 @@
-const Nav = () => {
+import Link from "next/link"
+import {CiSearch} from 'react';
+
+export default function Nav() {
+
   return (
-    <div>Nav</div>
+    <header className="w100 flex">
+      <h1 className="logo">.Plants</h1>
+      <nav>
+        <Link href='/home'>Home</Link>
+        <Link href='/shop'>Shop</Link>
+        <Link href='/about'>About</Link>
+      </nav>
+      <div className="header-right">
+        <span>{CiSearch}</span>
+      </div>
+    </header>
   )
 }
-
-export default Nav
